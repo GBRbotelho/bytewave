bf33cfa337c62f9aab276e1f1e2765c5cc930def
Gabriel Botelho <gabrielbbotelho05@gmail.com>
2024-02-05 01:29:51 -0300

    Container 2

--- src/containers/@Index.jsx
+++ src/containers/@Index.jsx
@@ -3,12 +3,14 @@ import React from "react";
 //Containers
 import Navbar from "./@Navbar";
 import Container1 from "./Container1";
+import Container2 from "./Container2";
 
 export default function Index() {
   return (
     <main>
       <Navbar />
       <Container1 />
+      <Container2 />
     </main>
   );
 }
---
+++ src/containers/Container2.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import styles from "./Container2.module.css";
+
+export default function Container2() {
+  return (
+    <section className={styles.section}>
+      <div className={styles.container}>
+        <h2 className={styles.title}>O que desenvolvemos?</h2>
+        <div className={styles.divCards}>
+          <div className={styles.card}>
+            <div></div>
+            <h2>landpages</h2>
+            <p>
+              sites que são focados em vendas especificas desenvolvidos em 1
+              unica pagina
+            </p>
+          </div>
+          <div className={styles.card}>
+            <div></div>
+            <h2>Institucionais</h2>
+            <p>
+              sites que são focados em vendas especificas desenvolvidos em 1
+              unica pagina
+            </p>
+          </div>
+          <div className={styles.card}>
+            <div></div>
+            <h2>Blogs e portais</h2>
+            <p>
+              sites que são focados em vendas especificas desenvolvidos em 1
+              unica pagina
+            </p>
+          </div>
+          <div className={styles.card}>
+            <div></div>
+            <h2>sistemas</h2>
+            <p>
+              sites que são focados em vendas especificas desenvolvidos em 1
+              unica pagina
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
