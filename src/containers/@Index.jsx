4edf4ef4e5e4ebe86ec9303acec60daa54721726
Gabriel Botelho <gabrielbbotelho05@gmail.com>
2024-02-05 17:47:08 -0300

    Container 6

--- src/containers/@Index.jsx
+++ src/containers/@Index.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import styles from "./@Index.module.css";
 
 //Containers
 import Navbar from "./@Navbar";
@@ -6,15 +7,17 @@ import Container1 from "./Container1";
 import Container3 from "./Container3";
 import Container4 from "./Container4";
 import Container5 from "./Container5";
+import Container6 from "./Container6";
 
 export default function Index() {
   return (
-    <main>
+    <main className={styles.main}>
       <Navbar />
       <Container1 />
       <Container3 />
       <Container4 />
       <Container5 />
+      <Container6 />
     </main>
   );
 }
---
+++ src/containers/Container6.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import styles from "./Container6.module.css";
+
+export default function Container6() {
+  return (
+    <section className={styles.section}>
+      <div className={styles.text}>
+        <p>
+          O que acha de ter um site que realmente te leve a um novo patamar no
+          seu negocio ?
+        </p>
+      </div>
+      <div className={styles.button}>
+        <button>Eu quero</button>
+      </div>
+    </section>
+  );
+}
