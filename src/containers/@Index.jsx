22dbadfeae2ff1a986e4e8bd0a002efb1cbec818
Gabriel Botelho <gabrielbbotelho05@gmail.com>
2024-02-07 18:01:25 -0300

    Container 2

--- src/containers/@Index.jsx
+++ src/containers/@Index.jsx
@@ -4,6 +4,7 @@ import styles from "./@Index.module.css";
 //Containers
 import Navbar from "./@Navbar";
 import Container1 from "./Container1";
+import Container2 from "./Container2";
 import Container3 from "./Container3";
 import Container4 from "./Container4";
 import Container5 from "./Container5";
@@ -17,6 +18,7 @@ export default function Index() {
     <main className={styles.main}>
       {/* <Navbar /> */}
       <Container1 />
+      <Container2 />
       <Container3 />;
       <Container4 />
       <Container5 />
---
+++ src/containers/Container2.jsx
@@ -0,0 +1,57 @@
+import React, { useEffect, useState } from "react";
+import styles from "./Container2.module.css";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Autoplay, Pagination, Navigation } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/scrollbar";
+
+export default function Container2() {
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+  const handleWindowResize = () => {
+    setWindowWidth(window.innerWidth);
+  };
+
+  useEffect(() => {
+    // Adiciona um ouvinte de evento para redimensionamento da janela
+    window.addEventListener("resize", handleWindowResize);
+  }, []);
+  return (
+    <section className={styles.section}>
+      <div className={styles.container}>
+        <Swiper
+          modules={[Navigation, Autoplay]}
+          slidesPerView={
+            (windowWidth > 1000 && 4) ||
+            (windowWidth > 650 && windowWidth <= 1000 && 3) ||
+            (windowWidth <= 650 && 2)
+          }
+          autoplay={{ delay: 1000 }}
+          loop={true}
+          className={styles.swiperContainer}
+        >
+          <SwiperSlide>
+            <p className={styles.p1}>Desenvolvimento</p>
+          </SwiperSlide>
+          <SwiperSlide>
+            <p className={styles.p2}>Tecnologia</p>
+          </SwiperSlide>
+          <SwiperSlide>
+            <p className={styles.p1}>Criatividade</p>
+          </SwiperSlide>
+          <SwiperSlide>
+            <p className={styles.p2}>Inovação</p>
+          </SwiperSlide>
+          <SwiperSlide>
+            <p className={styles.p1}>Compromisso</p>
+          </SwiperSlide>
+          <SwiperSlide>
+            <p className={styles.p2}>Visibilidade</p>
+          </SwiperSlide>
+        </Swiper>
+      </div>
+    </section>
+  );
+}
